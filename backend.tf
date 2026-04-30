ab813410a8202cd0a75505ce550594c6aff5c39d
Uxue <uxueex@gmail.com>
2026-04-30 16:02:44 +0200

    Aplicación GitHub Terraform

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "stterraformsecure"
-    container_name       = "tfstate"
-    key                  = "secureincident.tfstate"
-  }
-}
